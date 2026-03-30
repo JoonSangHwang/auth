6f5fe66a5c489e8f80a524d0ee980879623d9e8b
DESKTOP-QBCSIPG\HDMF <gufrus@naver.com>
2026-03-30 09:16:43 +0900

    init

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # member
+
+## 버전 정보
+
+| 항목 | 버전 |
+|---|---|
+| JDK | 1.8 |
+| Spring Boot | 2.7.18 |
+| Kotlin | 1.8.22 |
+| Gradle | 7.6.4 |
+| io.spring.dependency-management | 1.1.4 |
---
+++ auth/build.gradle.kts
@@ -0,0 +1,40 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.8.22"
+    kotlin("plugin.spring") version "1.8.22"
+    id("org.springframework.boot") version "2.7.18"
+    id("io.spring.dependency-management") version "1.1.4"
+}
+
+group = "com.hmdf"
+version = "0.0.1-SNAPSHOT"
+description = "auth"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs += "-Xjsr305=strict"
+        jvmTarget = "1.8"
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
